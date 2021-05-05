6e1337a6eb8e22129a2f8a78ab1b773043b26204
Ivar Abusdal <ivar.abusdal@designerkontoret.no>
2021-05-05 11:01:58 +0200

    Delete functionality.

--- OdeToFood/Pages/Resturants/Delete.cshtml.cs
+++ OdeToFood/Pages/Resturants/Delete.cshtml.cs
@@ -4,14 +4,42 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using OdeToFood.Core;
+using OdeToFood.Data;
 
 namespace OdeToFood.Pages.Resturants
 {
     public class DeleteModel : PageModel
     {
-        public void OnGet()
-        {
+        public Resturant Resturant { get; set; }
 
+        private readonly IResturantData resturantData;
+        public DeleteModel(IResturantData resturantData)
+        {
+            this.resturantData = resturantData;
+        }
+        public IActionResult OnGet(int resturantId)
+        {
+            Resturant = resturantData.GetById(resturantId);
+            if (Resturant == null)
+            {
+                return RedirectToPage("./NotFound");
+            }
+            return Page();
+        }
+
+        public IActionResult OnPost(int resturantId)
+        {
+            var resturant = resturantData.Delete(resturantId);
+            resturantData.Commit();
+
+            if (resturant == null)
+            {
+                return RedirectToPage("./NotFound");
+            }
+
+            TempData["Message"] = $"{resturant.Name} deleted.";
+            return RedirectToPage("./List");
         }
     }
 }
